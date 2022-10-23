b565b469c8f0f8ed38bfb86f55715f92a9fdae6a
ivivix90 <wiszenkoigor@gmail.com>
2022-10-23 14:29:10 +0200

    added deleting country funcionality + dev tools dependency to auto restart

--- src/main/resources/static/js/country.js
+++ src/main/resources/static/js/country.js
@@ -18,4 +18,12 @@ $('document').ready(function (){
         $('#editModal').modal();
 
     });
+    $('table #deleteButton').on('click', function (event){
+        event.preventDefault();
+        var href=$(this).attr('href');
+
+        $('#confirmDeleteButton').attr('href', href);
+
+        $('#deleteModal').modal();
+    });
 });
