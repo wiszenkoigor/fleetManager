2cfc80d47f365c9af9c6804d1fe95d42d10eb7f4
ivivix90 <wiszenkoigor@gmail.com>
2022-10-23 04:03:48 +0200

    countries modal

--- src/main/resources/static/js/country.js
+++ src/main/resources/static/js/country.js
@@ -1,6 +1,20 @@
 $('document').ready(function (){
     $('table #editButton').on('click', function (event) {
         event.preventDefault();
+
+        // /countries/findById/?id=1
+
+        var href = $(this).attr('href');
+
+        $.get(href, function(country, status) {
+            $('#idEdit').val(country.id);
+            $('#descriptionEdit').val(country.description);
+            $('#capitalEdit').val(country.capital);
+            $('#codeEdit').val(country.code);
+            $('#continentEdit').val(country.continent);
+            $('#nationalityEdit').val(country.nationality);
+        });
+
         $('#editModal').modal();
 
     });
